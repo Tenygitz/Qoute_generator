7e5a43c0aff13375b9b76032ffdb24b1b1e62c99
Tenygitz <tenybits@gmail.com>
2023-03-17 16:38:45 +0530

    changed

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Header.css';
+import {useSelector} from"react-redux";
 import { useNavigate } from 'react-router-dom';
+import Badge from '@mui/material/Badge';
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 
 function Header() {
     const navigate=useNavigate()
+    const {cardQoute}=useSelector((state)=>state.card)
   return (
     <div className='Header'>
         <h1 onClick={()=>navigate("/")}>Home </h1>
-        <h2 onClick={()=>navigate("bookmark")}>Bookmarks </h2>
+        <div onClick={()=>navigate("bookmark")}className='bookmark-nav'>
+        <h2 >Bookmarks </h2>
+        <Badge badgeContent={cardQoute.length} color="primary">
+      <BookmarkAddIcon className="Bookmark-Icon"  />
+    </Badge>
+    </div>
      </div>
   )
 }
